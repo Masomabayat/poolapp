d95ccdd9e1447826d87a9ff80bfb7ea9d7072a25
masomabayat <masoma.bayat2020@gmail.com>
2022-04-11 12:50:25 +0430

    add three choices

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 
 from .models import Choice, Question
-admin.site.register([Choice, Question])
+
+admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date informations', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register( Question, QuestionAdmin)
 
 # Register your models here.
